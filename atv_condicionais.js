70dcd9b5a9827cccf5144dddeb28d16583e7f1d0
Rayane Yasmin Dos Santos <rayaneyasmind@gmail.com>
2026-08-14 14:24:42 -0300

    Organizando arquivos de atividades

--- atv_condicionais.js
+++ atv_condicionais.js
@@ -233,39 +233,3 @@ for (const numero of numeros) {
 console.log("A soma total é:", soma); 
 
 //#endregion
-
-//#region Atividade 10 — Desafio do backend 
-/* 
-    Dado o array abaixo, mostre somente o pedidos pagos 
-
-    const pedidos = [
-    { cliente: "Ana", valor: 150, pago: true },
-    { cliente: "Carlos", valor: 300, pago: false },
-    { cliente: "Maria", valor: 500, pago: true },
-    { cliente: "João", valor: 200, pago: true },
-    { cliente: "Luizinho", valor: 100, pago: true },
-    { cliente: "Mel", valor: 243, pago: true },
-    { cliente: "ND da VA", valor: 2400, pago: false },
-    { cliente: "Ana", valor: 5400, pago: false },
-    { cliente: "Lebron ", valor: 1200, pago: true }
-];
-*/
-const pedidos = [
-  { cliente: "Bia", valor: 150, pago: true },
-  { cliente: "Bebel", valor: 300, pago: false },
-  { cliente: "Nathy", valor: 500, pago: true },
-  { cliente: "Lara", valor: 200, pago: true },
-  { cliente: "Vinicius", valor: 100, pago: true },
-  { cliente: "Isadora", valor: 243, pago: true },
-  { cliente: "Celina", valor: 2400, pago: false },
-  { cliente: "Michele", valor: 5400, pago: false },
-  { cliente: "Alessandro ", valor: 1200, pago: true }
-];
-
-for (const pedido of pedidos) {
-  if (pedido.pago) {
-    console.log(`Pedidos pagos - Clientes: ${pedido.cliente} | Valor: R$${pedido.valor}`);
-  }
-}
-
-//#endregion
